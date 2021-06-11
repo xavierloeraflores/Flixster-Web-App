d0c8cac738dcdb4332aa25e15510c035aef8b6bc
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-06-11 11:38:16 -0700

    Added Movie Trailer view and a home button

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# Flixster-Web-App
-Link: https://xavierloeraflores.github.io/Flixster-Web-App/
+## Week 1 Assignment: Flixster
+
+Submitted by: **NAME**
+
+Estimated time spent: **#** hours spent in total
+
+Deployed Application (optional): [Flixster Deployed Site](https://xavierloeraflores.github.io/Flixster-Web-App/)
+
+### Application Features
+
+#### REQUIRED FEATURES
+
+- [x] User can view a list of current movies from The Movie Database API as a grid view.
+- [x] For each movie displayed, user can see the following details: `Title`, `Poster Image`, `Votes`.
+- [x] User can load more current movies by clicking a button at the bottom of the list. The page should not refresh; movies should simply be added to the bottom.
+- [ ] Allow users to search for movies and display them in a grid view. Users should be able to clear results and view previous current movies displayed.
+- [ ] Website accounts for basic HTML/CSS accessibility features.
+- [ ] Website should be responsive.
+
+#### STRETCH FEATURES
+
+- [x] Deploy website using GitHub Pages. 
+- [x] Allow user to view more details about a movie within a popup.
+- [ ] Improve the user experience through CSS & animation.
+- [x] Allow movie video trailers to be played using [embedded YouTube](https://support.google.com/youtube/answer/171780?hl=en)
+- [ ] Implement anything else that you can get done to improve the app functionality!
+
+### Walkthrough Video
+
+`TODO://` Add the embedded URL code to your animated app walkthrough below, `ADD_EMBEDDED_CODE_HERE`. Make sure the video or gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after adding walkthrough video)
+
+`ADD_EMBEDDED_CODE_HERE`
+
+### Reflection
+
+* Did the topics discussed in your labs prepare you to complete the assignment? Be specific, which features in your weekly assignment did you feel unprepared to complete?
+
+Add your response here
+
+* If you had more time, what would you have done differently? Would you have added additional features? Changed the way your project responded to a particular event, etc.
+  
+Add your response here
+
+* Reflect on your project demo, what went well? Were there things that maybe didn't go as planned? Did you notice something that your peer did that you would like to try next time?
+
+Add your response here
+
+### Open-source libraries used
+
+- Add any links to open-source libraries used in your project.
+
+### Shout out
+
+Give a shout out to somebody from your cohort that especially helped you during your project. This can be a fellow peer, instructor, TA, mentor, etc.
--- index.js
+++ index.js
@@ -2,18 +2,62 @@
 /////////////          Declare Variables            /////////
 /////////////////////////////////////////////////////////////
 const api_key ="e715ac37017f63f7b72de0dfd9f91f58";
-let movies = document.querySelector("#movies");
-const btn = document.querySelector("#loadBtn");
+const loadBtn = document.querySelector("#loadBtn");
 const movie_page =document.getElementById("movie-page");
-let page=1;
-let closeBtn=movie_page;//Stops the errors
-let query=document.getElementById("searchInput");
+const movie_details =document.getElementById("movie-details");
 const searchBtn = document.getElementById("searchBtn");
+let closeBtn=document.getElementById("closeBtn");
+let movies = document.querySelector("#movies");
+let query=document.getElementById("searchInput");
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////          Declare Functions            ///////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+let page=1;
+let YOUTUBEID = 'M7lc1UVf-VE';
+
+
+
+/////////////////////////////////////////////////////////////
+/////////////             YOUTUBE                   /////////
+/////////////////////////////////////////////////////////////
+//YOUTUBE CODE https://developers.google.com/youtube/iframe_api_reference
+//YOUTUBE CODE https://developers.google.com/youtube/iframe_api_reference
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+let player; 
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: YOUTUBEID,
+      playerVars: {
+        'playsinline': 1
+      },
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+function onPlayerReady(event) {
+    // event.target.playVideo(); 
+
+    //This autoplays video if uncommented
+}
+let done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      done = true;
+    }
+  }
+function stopVideo() {
+    player.stopVideo();
+  }
+
 
 
 
@@ -22,20 +66,16 @@ const searchBtn = document.getElementById("searchBtn");
 /////////////             Popular                   /////////
 /////////////////////////////////////////////////////////////
 async function getPopularData() {
+    loadBtn.style.display="block";
+    // API CALLS
     const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=" + api_key + "&language=en-US&page="+page);
-
-
-
     const data = await response.json();
-
-
-
-    
+    //Calls movie data for each element
     data.results.forEach(element => {
 
         addMovies(element.title,element.poster_path, element.vote_average , element.id);
     });
-
+    //Gets movie page if clicked
     document.querySelectorAll(".movie").forEach(item =>{
         item.addEventListener("click", event =>{
             getMoviePage(item.firstChild.id)
@@ -47,23 +87,23 @@ function addMovies(title, path, score, movieId) {
     movies.innerHTML += ' <div class="movie"><img class="poster" alt="'+title+'" src="' + url +'"id="'+ movieId+'"/><div class="movie-bar"><span class= "movie-title">' + title + '</span><div class="rating"><img class= "star" alt="stars-png" src="https://www.freepnglogos.com/uploads/star-png/star-vector-png-transparent-image-pngpix-21.png"/><span class="score">'+score+ '</span></div></div></div>';
 }
 
+
+
 /////////////////////////////////////////////////////////////
 /////////////             Search                /////////
 /////////////////////////////////////////////////////////////
-
 async function getsearchData() {
-    // https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query=1page=1&include_adult=false
+
     let searchquery=query.value;
-    console.log("this" + searchquery)
+    //API Calls
     const response = await fetch("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&language=en-US&query="+ searchquery);
     const data = await response.json();
-    console.log(data);
-    data.results.forEach(element => {
-
+    //Add Movies to Grid
+    data.results.forEach(element => {  
         console.log(element.title,element.poster_path, element.vote_average , element.id);
         addMovies(element.title,element.poster_path, element.vote_average , element.id);
     });
-
+    //Allow each movie to open movie page
     document.querySelectorAll(".movie").forEach(item =>{
         item.addEventListener("click", event =>{
             getMoviePage(item.firstChild.id)
@@ -77,30 +117,40 @@ async function getsearchData() {
 /////////////             Movie Page                /////////
 /////////////////////////////////////////////////////////////
 async function getMoviePage(movieId){
-    console.log(movieId);
+    //Fetches Movie Data
     const response = await fetch("https://api.themoviedb.org/3/movie/"+ movieId+"?api_key=" + api_key + "&language=en-US&page="+page);
     const element = await response.json();
-    console.log(element)
-    let paths = [element.poster_path, element.backdrop_path];
-    generateMoviePage(paths, element.genres, element.title, element.runtime, element.release_date, element.vote_average,element.overview );
+    //Calls Video API & Sets YoutubeID
+    const videoresponse = await fetch("https://api.themoviedb.org/3/movie/"+ movieId+"/videos?api_key=" + api_key + "&language=en-US");
+    const videoelement = await videoresponse.json();
+    YOUTUBEID=videoelement.results[0].key;
+    player.loadVideoById(YOUTUBEID); 
+    //Generates Movie Page
+    console.log(element.genres)
+    let paths = [element.poster_path, element.backdrop_path]; 
+    generateMoviePage(paths, element.genres, element.title, element.runtime, element.release_date, element.vote_average,element.overview 
+        );
+
 }
 function generateMoviePage(paths,genre,title, runtime, release, score, description){
-    movie_page.style.display="flex";
+    movie_page.style.display="block";
     let poster=getImage(paths[0],"poster");
     let backdrop=getImage(paths[1],"wide");
-    let genres 
+    let genres=genre["0"].name;
+    console.log("Genre:"+genres);
 
-    movie_page.innerHTML='<button id="closeBtn">Close</button><div id="movie-details"><img class="backdrop" alt="' + title + ' backdrop" src="' + backdrop + '"><div id="info"><img class="poster" alt="' + title + 'movie poster" src="' + poster + '"><span class= "genre">' + genres + '</span><h2 class= "movie-title">' + title + '</span><span class= "run-time">' + runtime + '</span><span class="release-date">' + release + '</span><img class= "star" alt="stars-png" src="https://www.freepnglogos.com/uploads/star-png/star-vector-png-transparent-image-pngpix-21.png" /><span class="score">' + score + '</span></div><p class="description">' + description + '</p></div></div>';
+    movie_details.innerHTML='<img class="backdrop" alt="' + title + ' backdrop" src="' + backdrop + '"><div id="row2"><img class="poster" alt="' + title + 'movie poster" src="' + poster + '"><div id="info"><span class="genres">' + genres + '</span><h2 class= "movie-title">' + title + '</h2><div id="subrow"><span class= "run-time">' + runtime + ' min</span><span id="divider">|</span><span class="release-date">' + release + '</span></div><div id="rating"><img class= "star" alt="stars-png" src="https://www.freepnglogos.com/uploads/star-png/star-vector-png-transparent-image-pngpix-21.png" /><span class="score">' + score + '</span></div></div><span class="description">' + description + '</span></div>';
     closeBtn = document.getElementById("closeBtn");
     closeBtn.addEventListener("click", closeMoviePage);
+    
 }
 function closeMoviePage(){
 
-
+    stopVideo();
     document.getElementById("movie-page").style.display="none";
 }
 
-
+// https://api.themoviedb.org/3/movie/637649/video?api_key=e715ac37017f63f7b72de0dfd9f91f58&language=en-US
 
 function getImage(path, type){
     let imgWide ="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/";
@@ -129,19 +179,20 @@ function loadmore(){
 
 
 
+
 /////////////////////////////////////////////////////////////
 /////////////             Call Functions            /////////
 /////////////////////////////////////////////////////////////
 
 getPopularData();
-btn.addEventListener( "click", loadmore);
+loadBtn.addEventListener( "click", loadmore);
 closeBtn.addEventListener("click",closeMoviePage)
 
 searchBtn.addEventListener("click", function(event){
     event.preventDefault();
     console.log(query.value);
     document.getElementById("movies").innerHTML="";
+    loadBtn.style.display="none";
     getsearchData();
 });
-
 
